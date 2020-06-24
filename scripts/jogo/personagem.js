9d64d5055f3d3f750aa388611bd816c02bd8947d
Edson Maia <edsonmaia2005@hotmail.com>
2020-06-24 19:48:58 -0300

    Update personagem.js
    Ajustes no pulo
    Acréscimo dos métodos avanca e recua em personagem
    Mudança do método estaColidindo

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -7,10 +7,20 @@ class Personagem extends Animacao {
 		this.y = this.yInicial;
 
 		this.velocidadeDoPulo = 0;
-		this.gravidade 		  = 6;
+		this.gravidade 		  = 5.5;
 		this.alturaDoPulo 	  = -50;
 		this.pulos			  = 0;
 		this.quantMaximaPulos = 2;
+	}
+
+	// avançar 50px para a direita
+	avanca() {
+		this.x = this.x + 55;
+	}
+
+	// voltar 50px para a esquerda
+	recua() {
+		this.x = this.x - 60;
 	}
 
 	pula() {
@@ -31,18 +41,39 @@ class Personagem extends Animacao {
 	}
 	// x pers, y pers, larg, alt
 	estaColidindo(inimigo) {
-		const precisao = 0.7;
-		const colisao = collideRectRect(
-			this.x,
-			this.y,
-			this.largura * precisao,
-			this.altura  * precisao,
-			inimigo.x,
-			inimigo.y,
-			inimigo.largura * precisao,
-			inimigo.altura * precisao
-		);
-		return colisao;
+		//const precisao = 0.7;
+		noFill();
+		stroke('yellow');
+		strokeWeight(2);
+		
+		// personagem 			x, 			y, 	  d
+		let ripsta = circle(this.x+50, this.y+75, 85);
+	
+		// inimigo gotinha		x, 				y, 	  	 d
+		let gotinha = circle(inimigo.x+35, inimigo.y+25, 40);
+
+		// inimigo voador		x, 				y, 	  	 d
+		//let voador = circle(inimigo.x+60, inimigo.y+35, 45);
+		
+		// inimigo grande		x, 				y, 	  	 d
+		//let grande = circle(inimigo.x+130, inimigo.y+115, 85);
+
+		//elipse 1  (x, y, d, x2, y2, d2)
+		const colisaoGotinha  = collideCircleCircle(this.x+50,this.y+75,100, inimigo.x+35, inimigo.y+25, 40);
+		const colisaoVoadora  = collideCircleCircle(this.x+50,this.y+75,100, inimigo.x+60, inimigo.y+35, 45);
+		const colisaoGrande   = collideCircleCircle(this.x+50,this.y+75,100, inimigo.x+130, inimigo.y+115, 85);
+
+		const colisao = true;
+		// melhorar essa validacao, estou usando para ver quem esta colidindo
+		// estou usando inicialmente a colisao com a gotinha apenas
+		if (colisaoGotinha) {
+			return true;
+		} else if (colisaoVoadora) {
+			return false;
+		} else if (colisaoGrande) {
+			return false;
+		}
+		//return colisao;
 	}
 
 }
