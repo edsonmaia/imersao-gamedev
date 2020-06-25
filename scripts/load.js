adc0cc58f5c043447416208871d7024d5d3d3714
Edson Maia <edsonmaia2005@hotmail.com>
2020-06-25 11:20:04 -0300

    Update load.js

--- scripts/load.js
+++ scripts/load.js
@@ -1,13 +1,16 @@
 function preload() {
-  imagemCenario       = loadImage('imagens/cenario/floresta1.png');
-  imagemGameOver      = loadImage('imagens/assets/game-over.png');
+	imagemTelaInicial 	= loadImage('imagens/cenario/telaInicial.png');
+	imagemCenario       = loadImage('imagens/cenario/floresta1.png');
+	imagemGameOver      = loadImage('imagens/assets/game-over.png');
 
-  imagemPersonagem    = loadImage('imagens/personagem/correndo.png');
-  
-  imagemInimigo       = loadImage('imagens/inimigos/gotinha.png');
-  imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
-  imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
-  
-  somDoJogo           = loadSound('sons/trilha_jogo.mp3');
-  somDoPulo           = loadSound('sons/somPulo.mp3');
+	imagemPersonagem    = loadImage('imagens/personagem/correndo.png');
+
+	imagemInimigo       = loadImage('imagens/inimigos/gotinha.png');
+	imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
+	imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
+
+	fonteTelaInicial	= loadFont('imagens/assets/fonteTelaInicial.otf');
+
+	somDoJogo           = loadSound('sons/trilha_jogo.mp3');
+	somDoPulo           = loadSound('sons/somPulo.mp3');
 }
