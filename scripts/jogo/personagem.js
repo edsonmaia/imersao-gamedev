124c792674568167391c3b13d78463287279262f
Edson Maia <edsonmaia2005@hotmail.com>
2020-06-25 11:21:56 -0300

    Update personagem.js

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -47,16 +47,16 @@ class Personagem extends Animacao {
 		strokeWeight(2);
 		
 		// personagem 			x, 			y, 	  d
-		let ripsta = circle(this.x+50, this.y+75, 85);
+		// let ripsta = circle(this.x+50, this.y+75, 85);
 	
 		// inimigo gotinha		x, 				y, 	  	 d
-		let gotinha = circle(inimigo.x+35, inimigo.y+25, 40);
+		// let gotinha = circle(inimigo.x+35, inimigo.y+25, 40);
 
 		// inimigo voador		x, 				y, 	  	 d
-		let voador = circle(inimigo.x+60, inimigo.y+35, 45);
+		//let voador = circle(inimigo.x+60, inimigo.y+35, 45);
 		
 		// inimigo grande		x, 				y, 	  	 d
-		let grande = circle(inimigo.x+130, inimigo.y+115, 85);
+		//let grande = circle(inimigo.x+130, inimigo.y+115, 85);
 
 		//elipse 1  (x, y, d, x2, y2, d2)
 		const colisaoGotinha  = collideCircleCircle(this.x+50,this.y+75,100, inimigo.x+35, inimigo.y+25, 40);
