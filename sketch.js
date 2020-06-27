7c84d8748682b9e9d9713f88cde88ae9faed3bfe
Edson Maia <edsonmaia2005@hotmail.com>
2020-06-27 18:59:43 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,47 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(40);
+  somDoJogo.setVolume(0.15);
   somDoJogo.loop();
-  jogo        = new Jogo();
-  telaInicial = new TelaInicial();  
-  moedas      = new Moedas(); // MOEDAS
-  jogo.setup();
 
+  jogo         = new Jogo();
+  jogo2        = new Jogo2();
+  jogo3        = new Jogo3();
+  jogo4        = new Jogo4();
+  telaInicial  = new TelaInicial();
+  telaCreditos = new TelaCreditos();
+  moedas       = new Moedas(); // MOEDAS
+
+  jogo.setup();
+  
   cenas = {
     jogo,
-    telaInicial
+    telaInicial,
+    jogo2,
+    jogo3,
+    jogo4,
+    telaCreditos
   };
-
+  
   botaoGerenciador    = new BotaoGerenciador('Iniciar', width/2, height/2);
-  // BOTAO JOGAR NOVAMENTE
   botaoJogarNovamente = new BotaoJogarNovamente('Jogar Novamente', width/2, height/2);
+  botaoFase2          = new BotaoFase2('Próxima Fase', width/2, height/2, 'jogo2');
+  botaoFase3          = new BotaoFase3('Próxima Fase', width/2, height/2, 'jogo3');
+  botaoFase4          = new BotaoFase4('Próxima Fase', width/2, height/2, 'telaCreditos');
+  botaoCreditos       = new BotaoCreditos('Créditos', width/2, height/2);
+ 
+}
+
+// TOUCH {
+function touchStarted() {
+    personagem.pula();
+    somDoPulo.play();
 }
 
 function keyPressed() {
-  jogo.keyPressed(key);
+	jogo.keyPressed(key);
 }
 
 function draw() {
-  cenas[cenaAtual].draw();
-}
+	cenas[cenaAtual].draw();
+}	
